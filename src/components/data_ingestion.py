4bf7a01224d654973f9e0489159b0352a9c9b3a6
Mohamed Muradh <mohamedmuradh07@gmail.com>
2023-03-15 15:11:41 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 # store all the data inputs like for path to store train path or test path, store raw data for data ingestion in separate class
 @dataclass
 class DataIngestionConfig:
@@ -48,4 +51,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
